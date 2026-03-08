ea75a6b6eb0160c817298adf97e22952c37a7327
Prince Patra <princepatra9951@gmail.com>
2026-03-09 02:04:00 +0530

    Create ps1.js

---
+++ Practice Sets/Set 1/ps1.js
@@ -0,0 +1,26 @@
+// 1) Create a variable of type string and try to add a number to it.
+
+// let a = "TPP"
+// let sum = a + 9
+// console.log(sum)
+// Ans: TPP9
+
+
+// 2) Create a const object in  js. Can you change it to hold a number.
+
+// const a = {'name': "Prince", 'insta': '@theprincepatra'}
+// console.log(a)
+// a = 99
+// console.log(a)
+// Ans: {'name': "Prince", 'insta': '@theprincepatra'}
+//      error
+
+
+// 3) Add a new element in quesiton 2.
+
+// const a = {'name': "Prince", 'insta': '@theprincepatra'}
+// console.log(a)
+// a['github'] = 'theprincepatra'
+// console.log(a)
+// Ans: {'name': "Prince", 'insta': '@theprincepatra'}
+//      {'name': "Prince", 'insta': '@theprincepatra', 'github': 'theprincepatra'}
