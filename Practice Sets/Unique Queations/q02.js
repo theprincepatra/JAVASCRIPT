4b8cbc24717ae90345c0ee082eab3df147c50577
Prince Patra <princepatra9951@gmail.com>
2026-03-09 02:28:22 +0530

    Create q02.js

---
+++ Practice Sets/Unique Queations/q02.js
@@ -0,0 +1,19 @@
+// Find the output of this code:
+let day = 6;
+switch(day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      console.log("Weekday");
+      break;
+  case 6:
+  case 7:
+    console.log("Weekend");
+    break;
+  default:
+    console.log("Invalid");
+}
+
+// Ans: Weekend
