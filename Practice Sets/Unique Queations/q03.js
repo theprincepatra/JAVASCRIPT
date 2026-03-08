d1ccd0c9e2191eb2839a00b1cdb4e6c2506cb69e
Prince Patra <princepatra9951@gmail.com>
2026-03-09 02:37:39 +0530

    Create q03.js

---
+++ Practice Sets/Unique Queations/q03.js
@@ -0,0 +1,8 @@
+// Write a program to print "Try again" until the user enters the correct number.
+
+let num = 15
+let i = Number(prompt("Enter the number"))
+while(i===num){
+  console.log("Try again")
+}
+console.log("Enter the right number!!!")
